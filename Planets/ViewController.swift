6cfc2ae4b284dfb6fd68d3f648d00eb876b3393d
arrianne <arrianneoshea@hotmail.co.uk>
2020-04-29 17:30:28 +1000

    used the SCNAction class to get the sphere node to rotate. Had to add degreeToRadian function to do this

--- Planets/ViewController.swift
+++ Planets/ViewController.swift
@@ -15,6 +15,14 @@ class ViewController: UIViewController {
     // used to track orientation and position at all times
     let configuration = ARWorldTrackingConfiguration()
     
+    override func viewDidLoad() {
+       super.viewDidLoad()
+       // Do any additional setup after loading the view.
+       self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+       self.sceneView.session.run(configuration)
+           
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         let earth = SCNNode()
         let earthDay = #imageLiteral(resourceName: "Earth Day")
@@ -22,16 +30,15 @@ class ViewController: UIViewController {
         earth.geometry?.firstMaterial?.diffuse.contents = earthDay
         earth.position = SCNVector3(0,0,-1)
         self.sceneView.scene.rootNode.addChildNode(earth)
+        // SCNAction is an animation class which will change the structure or display of a node
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8 )
+        
+        earth.runAction(action)
         
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
-        self.sceneView.session.run(configuration)
-    }
-
-
 }
 
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
